dbd0653f1180346244c14b5043f2e82321495460
FellipePeixoto <rochapeduardo@gmail.com>
2020-03-20 19:38:40 -0300

    Initializer class to set up all game settings

---
+++ Assets/Scripts/Initialize/GameInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class GameInitializer
+{
+    static GameObject[] managers;
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void OnBeforeSceneLoadRuntimeMethod()
+    {
+        managers = Resources.LoadAll<GameObject>("Managers");
+
+        foreach(GameObject m in managers)
+        {
+            Object.Instantiate(m);
+        }
+    }
+}
