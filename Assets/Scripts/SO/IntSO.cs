addf68c26df60f86516d7d4cbfc18f310bd988a0
FellipePeixoto <rochapeduardo@gmail.com>
2020-03-20 11:23:27 -0300

    Merge Higor+Moyses+Fellipe

---
+++ Assets/Scripts/SO/IntSO.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "IntSO", menuName = "Scriptable/Vars/IntSO")]
+public class IntSO : ScriptableObject
+{
+    int ivalue;
+    public OnValueChangeDelegate OnValueChange;
+    public delegate void OnValueChangeDelegate(int newValue);
+
+    public int Value
+    {
+        get
+        {
+            return ivalue;
+        }
+        set
+        {
+            ivalue = value;
+            OnValueChange?.Invoke(ivalue);
+        }
+    }
+}
---
+++ Assets/Scripts/SO/VoidSO.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Void SO", menuName = "Scriptable/Vars/VoidSO")]
+public class VoidSO : ScriptableObject
+{
+    public OnRaiseDelegate onRaise;
+    public delegate void OnRaiseDelegate();
+
+    public void Raise()
+    {
+        onRaise?.Invoke();
+    }
+}
