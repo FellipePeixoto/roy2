020f17311b48e6a345196ae0d1ba67b095ac2c0e
FellipePeixoto <rochapeduardo@gmail.com>
2020-12-30 10:10:14 -0300

    prefab do pumper
    foi criado um script tbm, ainda n finalizado

---
+++ Assets/Scripts/Enemy/Pumper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pumper : MonoBehaviour
+{
+    [SerializeField] Vector3 _scanBoxHalfExtends = new Vector3(.5f, 1, .5f);
+    [SerializeField] float _offsetFromCenter = 3;
+    [SerializeField] Color _scanBoxDebbugColor = Color.red;
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = _scanBoxDebbugColor;
+
+        Gizmos.DrawWireCube(transform.position + Vector3.right * _offsetFromCenter, _scanBoxHalfExtends * 2);
+        Gizmos.DrawWireCube(transform.position + Vector3.left * _offsetFromCenter, _scanBoxHalfExtends * 2);
+    }
+}
