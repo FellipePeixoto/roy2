c6de8d76c52cc590e19f10fad9598690ddd30668
FellipePeixoto <rochapeduardo@gmail.com>
2021-01-13 09:28:14 -0300

    pumper implementado ainda nao testado

--- Assets/Scripts/Enemy/Pumper.cs
+++ Assets/Scripts/Enemy/Pumper.cs
@@ -2,15 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PumperState { Idle, OnTheWay, GetBack, Attacking }
+
 public class Pumper : MonoBehaviour
 {
+    [SerializeField] Transform _pumper;
+    [SerializeField] Transform _restPosition;
+    [SerializeField] Transform _lowPosition;
+    [SerializeField] LeanTweenType _easetype;
+    [SerializeField] float _timeToComplete;
     [SerializeField] Vector3 _scanBoxHalfExtends = new Vector3(.5f, 1, .5f);
     [SerializeField] float _offsetFromCenter = 3;
     [SerializeField] Color _scanBoxDebbugColor = Color.red;
+    [SerializeField] LayerMask _whereIsGas = (1 << 0);
+    [SerializeField] GasRobber _gasRobber;
+
+    bool _busy;
+    PumperState _currentState = PumperState.Idle;
 
     private void FixedUpdate()
     {
+        if (_busy)
+        {
 
+            return;
+        }
+
+        Collider[] colls = Physics.OverlapBox(transform.position, _scanBoxHalfExtends, Quaternion.identity, _whereIsGas);
+        if (colls.Length > 0)
+        {
+            StartCoroutine(Trigger());
+            StartCoroutine(TrySteal());
+            StartCoroutine(BackToNormal());
+        }
     }
 
     private void OnDrawGizmos()
@@ -19,5 +43,53 @@ public class Pumper : MonoBehaviour
 
         Gizmos.DrawWireCube(transform.position + Vector3.right * _offsetFromCenter, _scanBoxHalfExtends * 2);
         Gizmos.DrawWireCube(transform.position + Vector3.left * _offsetFromCenter, _scanBoxHalfExtends * 2);
+    }
+
+    IEnumerator Trigger() 
+    {
+        _busy = true;
+        _currentState = PumperState.OnTheWay;
+        LeanTween
+            .move(_pumper.gameObject, _lowPosition, _timeToComplete)
+            .setEase(_easetype);
+        yield return new WaitUntil(() => { return _pumper.transform.position.y == 0; });
+        Collider[] colls = Physics.OverlapBox(transform.position, _scanBoxHalfExtends, Quaternion.identity, _whereIsGas);
+        if (colls.Length > 0)
+        {
+            foreach(Collider col in colls)
+            {
+                _gasRobber.TryToStealGas(col.transform.GetInstanceID());
+            }
+            _currentState = PumperState.Attacking;
+        }
+        else
+        {
+            _currentState = PumperState.GetBack;
+        }
+    }
+
+    IEnumerator TrySteal()
+    {
+        yield return new WaitUntil(() => { return _currentState == PumperState.Attacking; });
+        Collider[] colls = Physics.OverlapBox(transform.position, _scanBoxHalfExtends, Quaternion.identity, _whereIsGas);
+        while (colls.Length > 0)
+        {
+            foreach (Collider col in colls)
+            {
+                _gasRobber.TryToStealGas(col.transform.GetInstanceID());
+            }
+            colls = Physics.OverlapBox(transform.position, _scanBoxHalfExtends, Quaternion.identity, _whereIsGas);
+            yield return new WaitForFixedUpdate();
+        }
+        _currentState = PumperState.GetBack;
+    }
+
+    IEnumerator BackToNormal()
+    {
+        yield return new WaitUntil(() => { return _currentState == PumperState.GetBack; });
+        LeanTween
+            .move(_pumper.gameObject, _restPosition, _timeToComplete)
+            .setEase(_easetype)
+            .setOnComplete(()=> { _busy = false; });
     }
 }
---
+++ Assets/Scripts/Player/GasContainer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void PumpgasHandler();
+
+public class GasContainer : MonoBehaviour
+{
+    public event PumpgasHandler OnPump;
+
+    public void Pick(float perSecond)
+    {        
+        OnPump?.Invoke();
+    }
+}
